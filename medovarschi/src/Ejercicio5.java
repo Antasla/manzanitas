b577bc97dfd6d77f245e5bcbc1c9b64631ee0925
[mdvrsch] <[dm.medovarschi.2020@alumnos.urjc.es]>
2022-03-31 17:17:31 +0200

    Ejercicio 5 OK

--- medovarschi/src/Ejercicio5.java
+++ medovarschi/src/Ejercicio5.java
@@ -5,18 +5,18 @@ import java.util.regex.Pattern;
 /*
     grupo 1 -> INFO
     grupo 2 -> main
-    grupo 3 -> Main
-    grupo 4 -> Starting ...
+    grupo 4 -> Main
+    grupo 5 -> Starting ...
  */
 
 public class Ejercicio5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String text = sc.nextLine();
-        Pattern p = Pattern.compile("\\b([INFO]).*([main]).*([A-Za-z]{1,})(.*\\n.*\\n.*)\\b");
+        Pattern p = Pattern.compile("([A-Za-z]{1,}).*\\[(.*)\\].*([.]|\\s)([A-Za-z0-9]{1,})\\s+[:]\\s+(.*)");
         Matcher m = p.matcher(text);
         while (m.find()) {
-            System.out.println(m.group(1) + "," + m.group(2) + "," + m.group(3) + "," + m.group(4));
+            System.out.println("\"" + m.group(1) + "\",\"" + m.group(2) + "\",\"" + m.group(4) + "\",\"" + m.group(5) + "\"");
         }
     }
 }
